bdb28efbe697d00cc3d9d77055185eb12ce4273e
Villadshammershoi <villadshammershoei@hotmail.com>
2016-09-16 13:47:16 +0200

    Added new functions.

--- billetflix/Scripts/PageScript/Scripts.js
+++ billetflix/Scripts/PageScript/Scripts.js
@@ -24,13 +24,31 @@
             console.log(data);
             $.each(data, function (key, item) {
                 console.log(item);
-                $('<div>', { class: "category-box col-sm-8 col-sm-offset-2 ", html: "<h1 class='pull-left'>" + CategoryText(item) + "</h1> <a class='pull-right'>Se alle</a>" })
-                    .append($("<div>", { class: "col-sm-9", html: EventText(item.Events) }))
+                $('<div>', { class: "category-box col-sm-8 col-sm-offset-2 ", html: "<h1 class='pull-left'>" + CategoryText(item) + "</h1> <a class='pull-right see-all-link' href='#'>Se alle</a>" })
+                    .append($("<div>", { class: "col-sm-12", html: EventText(item.Events, item.Name, item.Id) }))
                     .appendTo($("#CategoryContainer"));
                 console.log(item)
             })
         })
     };
+
+    function GetCategoryLinks() {
+        $(".category-container").empty();
+        $.ajax({
+            url: "http://localhost:48359/api/category/"
+        })
+        .done(function (data) {
+            console.log(data);
+            $.each(data, function (key, item) {
+                console.log(item);
+                $('<div>', { class: "col-md-3" })
+                    .append($("<a>", {class: "category-btn", href: "#", html: CategoryText(item), "data-id": item.Id  }))
+                    .appendTo($(".category-container"));
+                console.log(item)
+            })
+        })
+    };
+    GetCategoryLinks();
 
 
     $("#BtnCreateEvent").on("click", function () {
@@ -65,13 +83,36 @@
         return item.Name;
     }
 
-    function EventText(item) {
+    function EventText(item, categoryName) {
         console.log(item);
-        var eventContainer = $("<div>", { class: "event-list", "data-id": item.Id });
-        $.each(item, function (key, item) {
-            eventContainer.append($("<div>", { class: "col-sm-3 event-container", html: "<h4>" + item.Name + "</h4>" + "<p>" + item.Location.City + "</p>"})
-            .append($("<img>", { class: "img-responsive", src: item.Media[0].Url })))
+        var eventContainer = $("<div>", { class: "event-list col-sm-12", "data-id": item.Id });
+        $.each(item, function (key, event) {
+            console.log(event);
+            
+            var html = "<div class='col-sm-3 event-container'>\
+                            <div class='event-wrapper' data-id='" + event.FK_Category + "'>\
+                            <img class='img-responsive' src='" + event.Media[0].Url + "' />\
+                            <h4>" + event.Name + "</h4>\
+                            <p>" + event.Location.City + "</p>\
+                            <hr />\
+                            <a href='#'>" + categoryName + "</a>\
+                            </div>\
+                        </div>";
+            eventContainer.append(html);
         })
         return eventContainer;
     }
+
+
+    //fik jeg ikke til at virke... Kunne kun skjule alle categories, og ikke alle undtagen selected.
+    $(".category-btn").on("click", function () {
+        var selectedCategory = $(".category-btn").attr('data-id');
+
+        $('.category-box').css('display', 'none')
+        var selector = '.category-box';
+
+            selector += 'data-id="'+ selectedCategory + '"';
+
+        $(selector).fadeIn();
+    });
 });
